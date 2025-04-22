c467b9b1eee4d581d6459b35d75ca1a900723fe8
Ellis Ifoegbu <niceju4u@gmail.com>
2025-04-22 23:13:52 +0100

    More setups

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Let me tell you something");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Subscription Service App");
+});
+
+app.listen(3000, () => {
+  console.log("Subscription Service App is running on http://localhost:3000");
+});
+
+export default app;
