a3be952f7c32ca722b1dea17f089734b996342f2
Ellis Ifoegbu <niceju4u@gmail.com>
2025-05-08 23:03:51 +0100

    Implement middleware to authorize certain endpoints

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,50 @@
+import jwt from "jsonwebtoken";
+
+import { responseHandler } from "../utils/response.js";
+import { JWT_SECRET } from "../config/env.js";
+import { User } from "../models/user.model.js";
+
+export const authorize = async (req, res, next) => {
+  try {
+    let token;
+
+    let authHeader = req.headers.authorization || req.headers.Authorization;
+
+    if (authHeader && authHeader.startsWith("Bearer")) {
+      token = authHeader.split(" ")[1];
+    }
+
+    if (!token) {
+      return responseHandler(
+        res,
+        {
+          success: false,
+          message: "Unauthorized or missing token",
+        },
+        401
+      );
+    }
+
+    const decoded = jwt.verify(token, JWT_SECRET);
+
+    const user = await User.findById(decoded.userId);
+
+    if (!user) {
+      return responseHandler(
+        res,
+        { success: false, message: "Unauthorized" },
+        404
+      );
+    }
+
+    req.user = user;
+
+    next();
+  } catch (err) {
+    return responseHandler(
+      res,
+      { success: false, message: "Unauthorized", error: err.message },
+      404
+    );
+  }
+};
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { getUser, getUsers } from "../controllers/user.controller.js";
+import { authorize } from "../middlewares/auth.middleware.js";
 
 const userRouter = Router();
 
 userRouter.get("/", getUsers);
-userRouter.get("/:id", getUser);
+userRouter.get("/:id", authorize, getUser);
 
 export default userRouter;
